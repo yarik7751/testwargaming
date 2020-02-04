56a6188828547deea43181eb6aad81d010c76c00
yaroslavlevshunov <yaroslav.levshunov2@gmail.com>
2020-02-04 10:02:23 +0300

    start and base fragment

---
+++ feature-start/src/main/java/com/example/feature_start/StartFragment.kt
@@ -0,0 +1,11 @@
+package com.example.feature_start
+
+import android.view.View
+import com.example.presentation.BaseFragment
+
+class StartFragment : BaseFragment(R.layout.fragment_start) {
+
+    override fun initUi(view: View) {
+
+    }
+}
